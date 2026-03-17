edd56054e3ad44f817ce2d1d3c71600e6d630f0c
Sushhi-cdr <enzoseiti@gmail.com>
2026-03-17 16:39:32 -0300

    Criação do Models

---
+++ src/main/java/com/example/Ex5/Models/Categoria.java
@@ -0,0 +1,42 @@
+package com.example.Ex5.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nomeCategoria")
+    private String nomeCategoria;
+
+    @Column(name = "descricaoCategoria")
+    private String descricaoCategoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+
+    public String getDescricaoCategoria() {
+        return descricaoCategoria;
+    }
+
+    public void setDescricaoCategoria(String descricaoCategoria) {
+        this.descricaoCategoria = descricaoCategoria;
+    }
+}
